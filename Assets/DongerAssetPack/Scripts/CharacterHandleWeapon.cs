237cf5abb4ebabb0d6b6ac2c22349f2176fd6025
Dave Dong <fudaviddong@gmail.com>
2018-01-01 19:59:45 -0800

    Animation for Strafing begining with blend trees.

--- Assets/DongerAssetPack/Scripts/CharacterHandleWeapon.cs
+++ Assets/DongerAssetPack/Scripts/CharacterHandleWeapon.cs
@@ -10,14 +10,10 @@ namespace DongerAssetPack.IceEngine{
 	public class CharacterHandleWeapon : MonoBehaviour {
 
 		[Tooltip("This is the transfrom that the character will rotate around when aiming their weapon.")]
-		[SerializeField] bool _rotateArmsToTarget = false;
-		[SerializeField] Transform _spine;
 		protected Animator _anim;
 		protected CameraRaycaster _raycaster;
 		private const int WEAPONUP_LAYER_INDEX = 1;
 
-		LeftHandGrip _leftGrip;
-		RightHandGrip _rightGrip;
 		
 		[Tooltip("Used to determine if the left hand will grab the weapon.")]
 		[SerializeField] bool _useLeftHandGrip = false;
@@ -39,19 +35,11 @@ namespace DongerAssetPack.IceEngine{
 
 		protected virtual void Start()
 		{
-			//Find the hand grips on the weapon.
-			if (_useLeftHandGrip) _leftGrip = GetComponentInChildren<LeftHandGrip>();
-			if (_useRightHandGrip) _rightGrip = GetComponentInChildren<RightHandGrip>();
-
 			_anim = GetComponent<Animator>();
             _anim.SetLayerWeight(WEAPONUP_LAYER_INDEX, 1);
 
-			//Find the camera raycaster if the rotation is true.
-			if (_rotateArmsToTarget)
-			{
-				_raycaster = FindObjectOfType<CameraRaycaster>();
-				_raycaster.OnLayerHit += OnLayerHit;
-			}
+			_raycaster = FindObjectOfType<CameraRaycaster>();
+			_raycaster.OnLayerHit += OnLayerHit;
 		}
 
         private void OnLayerHit(RaycastHit hit)
@@ -59,55 +47,6 @@ namespace DongerAssetPack.IceEngine{
             _lookAtDirection = hit.point;
         }
 
-		void LateUpdate()
-        {
-            RotateSpine();
-        }
-
-		//TODO: remove later for debugging.
-        private void RotateSpine()
-        {
-            if (_targetIsGameObject)
-            {
-                var direction = (_target.transform.position - this.gameObject.transform.position).normalized;
-                //Move the spine instead.
-
-                _spine.up = -direction;
-
-                print("Forward Pos " + _spine.forward);
-            }
-        }
-
-        void OnAnimatorIK(int layerIndex)
-		{
-			if (_rotateArmsToTarget)
-			{
-				_anim.SetIKPosition(AvatarIKGoal.LeftHand, _leftGrip.transform.position);
-				_anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, _leftHandWeight);
-
-				_anim.SetIKPosition(AvatarIKGoal.RightHand, _rightGrip.transform.position);
-				_anim.SetIKPositionWeight(AvatarIKGoal.RightHand, _rightHandWeight);
-			}
-		}
-
-		void OnDrawGizmos()
-		{
-			// Gizmos.color = Color.green;
-			// Gizmos.DrawRay(_spine.transform.position, -_spine.up);
-
-			// Gizmos.color = Color.red;
-			// Gizmos.DrawRay(_spine.transform.position, -_spine.forward);
-
-			Gizmos.color = Color.red;
-			var testDirection = new Vector3(0, 0.5f, 0.5f);
-			Gizmos.DrawRay(this.transform.position, testDirection);
-
-			Gizmos.color = Color.red;
-			Gizmos.DrawRay(this.transform.position, Vector3.up);
-
-			Gizmos.color = Color.green;
-			var projection = Vector3.ProjectOnPlane(this.transform.position + testDirection, Vector3.up);
-		}
 	}
 }
 
